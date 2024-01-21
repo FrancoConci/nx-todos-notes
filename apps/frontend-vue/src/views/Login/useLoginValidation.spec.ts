ab5695c9be59eb8de492e7b829eaed2ff8522fa0
Franco <franco1conci@gmail..com>
2024-01-21 10:29:50 +0100

    FRONTEND-VUE Add useLoginValidation

--- apps/frontend-vue/src/views/Login/useLoginValidation.spec.ts
+++ apps/frontend-vue/src/views/Login/useLoginValidation.spec.ts
@@ -2,29 +2,53 @@ import { ref } from 'vue';
 import { useLoginValidation } from './useLoginValidation';
 
 describe('useLoginValidation', () => {
-  it('returns no error, then returns error if fields are invalid', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+  afterAll(() => {
+    vi.useRealTimers();
+  });
+
+  it('returns no error, if untouched', async () => {
     const username = ref('');
     const password = ref('');
     const { usernameErr, passwordErr } = useLoginValidation(username, password);
 
+    await vi.waitFor(() => vi.runAllTicks());
+
     // no error if fields are not touched
-    expect(usernameErr.value).toBeUndefined();
-    expect(passwordErr.value).toBeUndefined();
+    expect(usernameErr.value).toBe('');
+    expect(passwordErr.value).toBe('');
+  });
+  it('returns no error, if going from untouched to valid', async () => {
+    const username = ref('');
+    const password = ref('');
+    const { usernameErr, passwordErr } = useLoginValidation(username, password);
 
     // touch fields
     username.value = 'asd';
     password.value = 'asd';
 
-    // no error if fields are set to valid values
-    expect(usernameErr.value).toBeUndefined();
-    expect(passwordErr.value).toBeUndefined();
+    await vi.waitFor(() => vi.runAllTicks());
 
-    // set fields to valid values
+    // no error if fields are set to valid values
+    expect(usernameErr.value).toBe('');
+    expect(passwordErr.value).toBe('');
+  });
+  it('returns error, if fields go from valid to invalid', async () => {
+    const username = ref('asd');
+    const password = ref('asd');
+    const { usernameErr, passwordErr } = useLoginValidation(username, password);
+
+    // set fields to invalid values
     username.value = '';
     password.value = '';
 
+    await vi.waitFor(() => vi.runAllTicks());
     // errors if fields are touched and invalid
-    expect(usernameErr.value).toBeUndefined();
-    expect(passwordErr.value).toBeUndefined();
+    expect(usernameErr.value).not.toBeUndefined();
+    expect(usernameErr.value).not.toBe('');
+    expect(passwordErr.value).not.toBeUndefined();
+    expect(passwordErr.value).not.toBe('');
   });
 });
--- apps/frontend-vue/src/views/Login/useLoginValidation.ts
+++ apps/frontend-vue/src/views/Login/useLoginValidation.ts
@@ -4,14 +4,15 @@ export const useLoginValidation = (
   username: Ref<string>,
   password: Ref<string>
 ) => {
-  const usernameErr = ref();
-  const passwordErr = ref();
+  const usernameErr = ref('');
+  const passwordErr = ref('');
 
   watch(username, () => {
-    usernameErr.value = username.value ? null : 'i am an error';
+    console.log('username.value', username.value);
+    usernameErr.value = username.value ? '' : 'i am an error';
   });
   watch(password, () => {
-    passwordErr.value = password.value ? null : 'i am an error';
+    passwordErr.value = password.value ? '' : 'i am an error';
   });
 
   return { usernameErr, passwordErr };
